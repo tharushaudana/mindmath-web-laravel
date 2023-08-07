404aaa3e5335c65b1b949ef911d27cf5de502a27
Tharusha Udana <tharushaudana519@gmail.com>
2023-08-07 22:46:15 +0530

    update

--- resources/views/student/test.blade.php
+++ resources/views/student/test.blade.php
@@ -1,9 +1,9 @@
 @extends('student.student')
 
 @section('content')
-    <div class="w-100 h-100" style="position: relative; background-color: #EDE4FF;">
+    <div class="w-100 h-100" style="position: relative; background-image: url('{{ asset('assets/student/images/bg3.jpg') }}'); background-color: rgba(237,228,255,1); background-blend-mode: lighten;">
         <div style="height: 10px;"></div>
-        <div class="px-4 py-2 w-100 d-flex d-flex justify-content-between align-items-center" style="background-color: #FFE5AD; border: 1px solid #6F61C0; color: #6528F7;">
+        <div class="px-4 py-2 w-100 d-flex justify-content-between align-items-center" style="background-color: #FFE5AD; border: 1px solid #6F61C0; color: #6528F7;">
             <div>
                 <span style="font-size: 10px;">Mind<b>Math</b> MCQ Engine</span>
                 <br>
@@ -24,10 +24,18 @@
                     dsds
                 </div>
             </div>
+            <div class="h-100 py-4" style="position: absolute; width: 75%; transform: translateY(6px);">
+                <div style="height: 20px; background-color: #6F61C0; width: 50px; transform: translateX(-7px);">
+                    <div class="w-100" style="background-color: #A084E8; height: 6px; border-bottom-left-radius: 15px; border-top-left-radius: 15px; transform: translateY(-3px);"></div>
+                </div>
+            </div>
             <div class="card h-100 rounded-5 py-3 px-3" style="position: absolute; width: 75%; box-shadow: 0 1px 1px rgba(0,0,0,0.12), 0 2px 2px rgba(0,0,0,0.12);">
                 <div class="card-body">
-                    <div class="p-3 rounded-4" style="background-color: #6F61C0; color: white; box-shadow: 0px 5px 10px 0px #6F61C0; transform: translateX(-50px);">
-                        <span style="font-size: 14px; opacity: 0.8;"><i class="fa-solid fa-stopwatch"></i>&nbsp;&nbsp;5 secs</span>
+                    <div class="p-3" style="background-color: #6F61C0; color: white; box-shadow: 0px 5px 10px 0px #6F61C0; transform: translateX(-40px); width: calc(100% + 40px); border-top-right-radius: 10px; border-bottom-right-radius: 10px; border-bottom-left-radius: 4px;">
+                        <div class="w-100 d-flex justify-content-between align-items-center">
+                            <span style="font-size: 14px; opacity: 0.8;"><i class="fa-solid fa-stopwatch"></i>&nbsp;&nbsp;5 secs</span>
+                            <span style="font-size: 14px; opacity: 0.8;"><b>10</b> of 40</span>
+                        </div>
                         <div class="mt-2"></div>
                         What is the answer? 
                         <div style="font-weight: bold;">
@@ -68,7 +76,7 @@
                 </div>
             </div>
         </div>
-        <div class="w-100" style="position: absolute; bottom: 40px; display: flex; justify-content: center; align-items: center;">
+        <div class="w-100" style="position: absolute; bottom: 20px; display: flex; justify-content: center; align-items: center;">
             <div class="rounded-pill" style="width: 180px; height: 60px; border: 1px solid #6F61C0; padding: 1px 4px;">
                 <div class="rounded-pill" style="width: 100%; height: 100%; border: 1px solid #6F61C0; padding: 1px 4px;">
                     <div class="rounded-pill" style="width: 100%; height: 100%; border: 1px solid #6F61C0; padding: 1px 4px;">
