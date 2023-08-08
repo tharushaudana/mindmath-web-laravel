8b79bd450a6555bc473ceae17acf4fa7d76120ad
Tharusha Udana <tharushaudana519@gmail.com>
2023-08-08 05:35:05 +0530

    update

--- app/Http/Controllers/student/TestController.php
+++ app/Http/Controllers/student/TestController.php
@@ -11,21 +11,38 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     public function index(Test $test) {
-        /*$nq = 40;
-        $oo = ['+', '*', '/', '/', '/', '*', '-'];
-        $do = [2, 2, 3, 2, 2, 1, 2, 1];
-        //$oo = ['/'];
-        //$do = [1,1];
+        $previousOpOrders = [];
+        $previousNumberSets = [];
+
+        $nq = 82;
+        //$oo = ['+', '*', '/', '/', '/', '*', '-'];
+        //$do = [2, 2, 3, 2, 2, 1, 2, 1];
+        $oo = ['/'];
+        $do = [1,1];
         $io = true;
         $soo = true;
         $sdo = true;
+
+        $fails = 0;
 
         //dd(ENGINE_AUTOMCQ::isOrdersValidForOnlyIntegerAnswers(['+', '/', '/', '/', '-'], [9, 3, 3, 3, 3, 3]));
 
         $test = [];
 
         for ($i=0; $i < $nq; $i++) {             
-            $q = ENGINE_AUTOMCQ::generateQuestion($oo, $do, $soo, $sdo, 1, 4);
+            $fails = 0;
+
+            do {
+                $q = ENGINE_AUTOMCQ::generateQuestion($oo, $do, $soo, $sdo, 1, 4);
+                $fails++;
+            } while (in_array($q[0], $previousOpOrders) && in_array($q[1], $previousNumberSets));
+
+            //if ($fails >= 1000) break;
+
+            array_push($previousOpOrders, $q[0]);
+            array_push($previousNumberSets, $q[1]);
+
+            //$q = ENGINE_AUTOMCQ::generateQuestion($oo, $do, $soo, $sdo, 1, 4);
             $answers = $q[2];
 
             $exp = $this->showExpression($q[0], $q[1]);
@@ -38,9 +55,11 @@ class TestController extends Controller
             array_push($test, $exp);
         }
 
-        dd($test);*/
+        dd($test);
 
-        return view('student.test');
+        //dd($this->generateExpressions($oo, $do));
+
+        //return view('student.test');
     }
 
     public function showExpression($operations, $numbers) {
@@ -53,5 +72,48 @@ class TestController extends Controller
         }
 
         return $exp;
+    }
+
+    function evaluateExpression($expression) {
+        return eval("return $expression;");
+    }
+    
+    function generateExpressions($operationsOrder, $digitSizeOrder) {
+        $totalExpressions = 0;
+        $numDigits = count($digitSizeOrder);
+        $numOperators = count($operationsOrder);
+        
+        for ($i = 0; $i <= $numDigits; $i++) {
+            for ($j = 0; $j <= $numOperators; $j++) {
+                if ($i + $j == $numDigits) {
+                    $numOperands = $i + 1;
+                    $numOperatorsUsed = $j;
+                    $numEmptySlots = $numOperands + $numOperatorsUsed - 1;
+    
+                    $combinations = $this->nCr($numEmptySlots, $numOperands - 1);
+    
+                    for ($k = 0; $k < $combinations; $k++) {
+                        $expression = '';
+                        for ($m = 0; $m < $numOperands - 1; $m++) {
+                            $expression .= $digitSizeOrder[$m] . $operationsOrder[$k % $numOperatorsUsed];
+                        }
+                        $expression .= $digitSizeOrder[$numOperands - 1];
+                        
+                        if ($this->evaluateExpression($expression) % 1 === 0) {
+                            $totalExpressions++;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return $totalExpressions;
+    }    
+
+    function nCr($n, $r) {
+        if ($r == 0 || $n == $r) {
+            return 1;
+        }
+        return $this->nCr($n - 1, $r - 1) + $this->nCr($n - 1, $r);
     }
 }
